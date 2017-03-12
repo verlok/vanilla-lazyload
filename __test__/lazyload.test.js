703559b9a21de29902cf8f709ce47900c71f33d5
Andrea Verlicchi <andrea.verlicchi@gmail.com>
2017-03-12 21:26:15 +0100

    Testing queryOriginNode

--- __test__/lazyload.test.js
+++ __test__/lazyload.test.js
@@ -9,58 +9,72 @@ test("Public methods", () => {
     });
 });
 
-test("Constructor with default options", () => {
-    const ll = new LazyLoad();
-    expect(ll._settings).toEqual({
-        elements_selector: "img",
-        container: window,
-        threshold: 300,
-        throttle: 150,
-        data_src: "original",
-        data_srcset: "original-set",
-        class_loading: "loading",
-        class_loaded: "loaded",
-        skip_invisible: true,
-        callback_load: null,
-        callback_error: null,
-        callback_set: null,
-        callback_processed: null
+describe("Constructor", () => {
+    test("...with default options", () => {
+        const ll = new LazyLoad();
+        expect(ll._settings).toEqual({
+            elements_selector: "img",
+            container: window,
+            threshold: 300,
+            throttle: 150,
+            data_src: "original",
+            data_srcset: "original-set",
+            class_loading: "loading",
+            class_loaded: "loaded",
+            skip_invisible: true,
+            callback_load: null,
+            callback_error: null,
+            callback_set: null,
+            callback_processed: null
+        });
+    });
+
+    test("...with custom options", () => {
+        const ll = new LazyLoad({
+            data_src: "data-src",
+            data_srcset: "data-srcset"
+        });
+        expect(ll._settings).toEqual({
+            elements_selector: "img",
+            container: window,
+            threshold: 300,
+            throttle: 150,
+            data_src: "data-src",
+            data_srcset: "data-srcset",
+            class_loading: "loading",
+            class_loaded: "loaded",
+            skip_invisible: true,
+            callback_load: null,
+            callback_error: null,
+            callback_set: null,
+            callback_processed: null
+        });
+    });
+
+    test("...of different instances", () => {
+        const ll1 = new LazyLoad({
+            elements_selector: ".lazy1 img",
+            data_src: "data-src",
+            data_srcset: "data-srcset"
+        });
+        const ll2 = new LazyLoad({
+            elements_selector: ".lazy2 img",
+            data_src: "data-src",
+            data_srcset: "data-srcset"
+        });
+        expect(ll1._settings.elements_selector).toBe(".lazy1 img");
+        expect(ll2._settings.elements_selector).toBe(".lazy2 img");
     });
 });
 
-test("Constructor with custom options", () => {
-    const ll = new LazyLoad({
-        data_src: "data-src",
-        data_srcset: "data-srcset"
-    });
-    expect(ll._settings).toEqual({
-        elements_selector: "img",
-        container: window,
-        threshold: 300,
-        throttle: 150,
-        data_src: "data-src",
-        data_srcset: "data-srcset",
-        class_loading: "loading",
-        class_loaded: "loaded",
-        skip_invisible: true,
-        callback_load: null,
-        callback_error: null,
-        callback_set: null,
-        callback_processed: null
-    });
-});
-
-test("Two instances must differ", () => {
-    const ll1 = new LazyLoad({
-        elements_selector: ".lazy1 img",
-        data_src: "data-src",
-        data_srcset: "data-srcset"
-    });
+test("QueryOriginNode is valid", () => {
+    const scrollArea = document.createElement('div');
+    scrollArea.classList.add('scrollArea');
+    window.document.documentElement.appendChild(scrollArea);
+    const ll1 = new LazyLoad();
     const ll2 = new LazyLoad({
-        elements_selector: ".lazy2 img",
-        data_src: "data-src",
-        data_srcset: "data-srcset"
+        container: scrollArea
     });
-    expect(ll1._settings.elements_selector).toBe(".lazy1 img");
-    expect(ll2._settings.elements_selector).toBe(".lazy2 img");
+    expect(ll1._queryOriginNode.nodeType).toBe(9);
+    expect(ll2._queryOriginNode.nodeType).toBe(1);
 });
