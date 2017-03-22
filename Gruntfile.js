555fccd0acdf1fbf1a07d96cadd03dcc9bbef85a
Andrea Verlicchi <verlicchia@git.yoox.net>
2017-03-22 09:25:37 +0100

    Updated Gruntfile and Package to use Babel

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,13 +2,24 @@ module.exports = function(grunt) {
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		babel: {
+			options: {
+				sourceMap: false,
+				presets: ['es2015']
+			},
+			dist: {
+				files: {
+                	'dist/lazyload.transpiled.js': 'src/lazyload.js'
+				}
+			}
+		},
 		uglify: {
 			options: {
-				banner: '<%= banner %>',
+				banner: '',
 				sourceMap: true
 			},
 			dist: {
-				src: 'src/lazyload.js',
+				src: 'dist/lazyload.transpiled.js',
 				dest: 'dist/lazyload.min.js'
 			}
 		},
@@ -23,19 +34,21 @@ module.exports = function(grunt) {
 					console: true,
 					module: true,
 					document: true
-				}
+				},
+				"esnext": true
 			}
 		},
 		watch: {
 			files: ['<%= jshint.files %>'],
-			tasks: ['jshint', 'uglify']
+			tasks: ['jshint', 'babel', 'uglify']
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-babel');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.registerTask('default', ['jshint', 'uglify', 'watch']);
+	grunt.registerTask('default', ['jshint', 'babel', 'uglify', 'watch']);
 
 };
