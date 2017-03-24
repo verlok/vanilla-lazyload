485963a77517223e4718b281407d1fdd5cedf455
Andrea Verlicchi <verlicchia@git.yoox.net>
2017-03-24 15:20:01 +0100

    Tested setSources for image

--- __test__/lazyload.test.js
+++ __test__/lazyload.test.js
@@ -164,6 +164,9 @@ describe("_setSourcesForPicture", () => {
     let img100 = "http://placehold.it/100x100";
     let img200 = "http://placehold.it/200x200";
     let img400 = "http://placehold.it/400x400";
+    let dataSrcSetPartialAttr = 'original-set'; 
+    let dataSrcSetAttr = 'data-' + dataSrcSetPartialAttr;
+    let srcsetAttr = 'srcset';
     beforeEach(() => {
         p = document.createElement('picture');
         p.appendChild(s1 = document.createElement('source'));
@@ -171,28 +174,79 @@ describe("_setSourcesForPicture", () => {
         p.appendChild(i = document.createElement('img'));
     });
     test("...with initially empty srcset", () => {
-        s1.setAttribute('data-original-set', img200);
-        s2.setAttribute('data-original-set', img400);
-        testFunct(i, 'original-set');
-        expect(s1.getAttribute('srcset')).toBe(img200);
-        expect(s2.getAttribute('srcset')).toBe(img400);
+        s1.setAttribute(dataSrcSetAttr, img200);
+        s2.setAttribute(dataSrcSetAttr, img400);
+        testFunct(i, dataSrcSetPartialAttr);
+        expect(s1.getAttribute(srcsetAttr)).toBe(img200);
+        expect(s2.getAttribute(srcsetAttr)).toBe(img400);
     });
     test("...with initial value in srcset", () => {
-        s1.setAttribute('srcset', img1);
-        s2.setAttribute('srcset', img1);
-        s1.setAttribute('data-original-set', img200);
-        s2.setAttribute('data-original-set', img400);
-        testFunct(i, 'original-set');
-        expect(s1.getAttribute('srcset')).toBe(img200);
-        expect(s2.getAttribute('srcset')).toBe(img400);
+        s1.setAttribute(srcsetAttr, img1);
+        s2.setAttribute(srcsetAttr, img1);
+        s1.setAttribute(dataSrcSetAttr, img200);
+        s2.setAttribute(dataSrcSetAttr, img400);
+        testFunct(i, dataSrcSetPartialAttr);
+        expect(s1.getAttribute(srcsetAttr)).toBe(img200);
+        expect(s2.getAttribute(srcsetAttr)).toBe(img400);
     });
     test("...with initial value in srcset and empty data-srcset", () => {
-        s1.setAttribute('srcset', img1);
-        s2.setAttribute('srcset', img1);
-        s1.setAttribute('data-original-set', "");
-        s2.setAttribute('data-original-set', "");
-        testFunct(i, 'original-set');
-        expect(s1.getAttribute('srcset')).toBe(img1);
-        expect(s2.getAttribute('srcset')).toBe(img1);
+        s1.setAttribute(srcsetAttr, img1);
+        s2.setAttribute(srcsetAttr, img1);
+        s1.setAttribute(dataSrcSetAttr, "");
+        s2.setAttribute(dataSrcSetAttr, "");
+        testFunct(i, dataSrcSetPartialAttr);
+        expect(s1.getAttribute(srcsetAttr)).toBe(img1);
+        expect(s2.getAttribute(srcsetAttr)).toBe(img1);
     });
 });
+
+describe("_setSources for image", () => {
+    let i;
+    let fakeInstance = {_setSourcesForPicture: jest.fn()};
+    let testFunct = LazyLoad.prototype._setSources.bind(fakeInstance);
+    let img1 = "http://placehold.it/1x1";
+    let img100 = "http://placehold.it/100x100";
+    let img200 = "http://placehold.it/200x200";
+    let img400 = "http://placehold.it/400x400";
+    let dataSrcPartialAttr = 'original'; 
+    let dataSrcSetPartialAttr = 'original-set'; 
+    let dataSrcAttr = 'data-' + dataSrcPartialAttr;
+    let dataSrcSetAttr = 'data-' + dataSrcSetPartialAttr;
+    let srcAttr = 'src';
+    let srcSetAttr = 'srcset';
+    beforeEach(() => {
+        i = document.createElement('img');
+    });
+    test("...with initially empty src and srcset", () => {
+        fakeInstance._setSourcesForPicture.mockClear();
+        i.setAttribute(dataSrcAttr, img200);
+        i.setAttribute(dataSrcSetAttr, img400);
+        testFunct(i, dataSrcSetPartialAttr, dataSrcPartialAttr);
+        expect(i.getAttribute(srcAttr)).toBe(img200);
+        expect(i.getAttribute(srcSetAttr)).toBe(img400);
+        expect(fakeInstance._setSourcesForPicture).toHaveBeenCalledTimes(1);
+    });
+    test("...with initial values in src and srcset", () => {
+        fakeInstance._setSourcesForPicture.mockClear();
+        i.setAttribute(srcAttr, img1);
+        i.setAttribute(srcSetAttr, img1);
+        i.setAttribute(dataSrcAttr, img200);
+        i.setAttribute(dataSrcSetAttr, img400);
+        testFunct(i, dataSrcSetPartialAttr, dataSrcPartialAttr);
+        expect(i.getAttribute(srcAttr)).toBe(img200);
+        expect(i.getAttribute(srcSetAttr)).toBe(img400);
+        expect(fakeInstance._setSourcesForPicture).toHaveBeenCalledTimes(1);
+    });
+    test("...with initial values in src and srcset and empty data-*", () => {
+        fakeInstance._setSourcesForPicture.mockClear();
+        i.setAttribute(srcAttr, img200);
+        i.setAttribute(srcSetAttr, img400);
+        i.setAttribute(dataSrcAttr, "");
+        i.setAttribute(dataSrcSetAttr, "");
+        testFunct(i, dataSrcSetPartialAttr, dataSrcPartialAttr);
+        expect(i.getAttribute(srcAttr)).toBe(img200);
+        expect(i.getAttribute(srcSetAttr)).toBe(img400);
+        expect(fakeInstance._setSourcesForPicture).toHaveBeenCalledTimes(1);
+    });
+});
+
