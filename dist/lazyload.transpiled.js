5ee1dbeaad3e05ad194684d788cea1eeb301afaa
Andrea Verlicchi <verlicchia@git.yoox.net>
2017-04-06 22:43:56 +0200

    New dist with transpiled object.assign

--- dist/lazyload.transpiled.js
+++ dist/lazyload.transpiled.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
@@ -78,7 +80,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
         function LazyLoad(instanceSettings) {
             _classCallCheck(this, LazyLoad);
 
-            this._settings = Object.assign({}, _defaultSettings, instanceSettings);
+            this._settings = _extends({}, _defaultSettings, instanceSettings);
             this._queryOriginNode = this._settings.container === window ? document : this._settings.container;
 
             this._previousLoopTime = 0;
