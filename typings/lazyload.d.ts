2d34128b4489e9915fc9d9f02d27fb2cc45e2611
Andrea Verlicchi <verlicchia@git.yoox.net>
2017-04-07 06:30:05 +0200

    Updated typings

--- typings/lazyload.d.ts
+++ typings/lazyload.d.ts
@@ -7,10 +7,12 @@
     data_srcset?: string;
     class_loading?: string;
     class_loaded?: string;
+    class_error?: string;
     skip_invisible?: boolean;
     show_while_loading?: boolean;
     callback_set?: (elt:HTMLImageElement) => void;
     callback_load?: (elt:HTMLImageElement) => void;
+    callback_error?: (elt:HTMLImageElement) => void;
     callback_processed?: (elts:HTMLImageElement[]) => void;
     placeholder?: string;
 }
