0eb8ca9893da44948e5e88f810fec37e88f4fd2d
Andrea <andrea.verlicchi@ynap.com>
2017-08-28 22:32:22 +0200

    Fixed UMD undefined -> this

--- dist/lazyload.js
+++ dist/lazyload.js
@@ -1,12 +1,10 @@
-'use strict';
-
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
 (function (global, factory) {
     (typeof exports === 'undefined' ? 'undefined' : _typeof(exports)) === 'object' && typeof module !== 'undefined' ? module.exports = factory() : typeof define === 'function' && define.amd ? define(factory) : global.LazyLoad = factory();
-})(undefined, function () {
+})(this, function () {
     'use strict';
 
     var defaultSettings = {
