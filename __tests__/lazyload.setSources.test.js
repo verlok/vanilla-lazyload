c2eb462eb7716bb5257ccb98dc825a2069d4e7d4
Andrea Verlicchi <andrea.verlicchi@gmail.com>
2017-08-30 20:28:47 +0200

    Tested iframe

--- __tests__/lazyload.setSources.test.js
+++ __tests__/lazyload.setSources.test.js
@@ -1,8 +1,141 @@
 import setSources from "../src/lazyLoad.setSources";
 
+const settings = {
+    data_src: "original",
+    data_srcset: "originalSet"
+};
+
+const _expectAttr = (el, attr, val) => {
+    expect(el.getAttribute(attr)).toBe(val);
+};
+
+expect.extend({
+    toHaveAttribute: (element, attributeName, valueToVerify) => {
+        const actualValue = element.getAttribute(attributeName);
+        return actualValue === valueToVerify ? {
+            message: () => `${element.tagName} has attribute "${attributeName}" set to "${valueToVerify}"`,
+            pass: true
+        } : {
+            message: () => `expected ${element.tagName} to have attribute "${attributeName}" set to "${valueToVerify}", received "${actualValue}"`,
+            pass: false
+        }
+    }
+});
+
 test("setSources is defined", () => {
     expect(typeof setSources).toBe("function");
 });
+
+describe("setSources for image", () => {
+    let img;
+    let img1 = "http://placehold.it/1x1";
+    let img200 = "http://placehold.it/200x200";
+    let img400 = "http://placehold.it/400x400";
+
+    beforeEach(() => {
+        img = document.createElement("img");
+        // Parent is a div
+        let div = document.createElement("div");
+        div.appendChild(img);
+    });
+
+    test("...with initially empty src and srcset", () => {
+        img.dataset = {
+            "original": img200,
+            "originalSet": img400
+        };
+        setSources(img, settings);
+        expect(img).toHaveAttribute("src", img200);
+        expect(img).toHaveAttribute("srcset", img400);
+    });
+
+    test("...with initial values in src and srcset", () => {
+        img.dataset = {
+            "original": img200,
+            "originalSet": img400
+        };
+        img.setAttribute("src", img1);
+        img.setAttribute("srcset", img1);
+        setSources(img, settings);
+        expect(img).toHaveAttribute("src", img200);
+        expect(img).toHaveAttribute("srcset", img400);
+    });
+    test("...with initial values in src and srcset and empty data-*", () => {
+        img.dataset = {
+            "original": "",
+            "originalSet": ""
+        };
+        img.setAttribute("src", img200);
+        img.setAttribute("srcset", img400);
+        setSources(img, settings);
+        expect(img).toHaveAttribute("src", img200);
+        expect(img).toHaveAttribute("srcset", img400);
+    });
+});
+
+describe("_setSources for iframe", () => {
+    let iframe;
+    let srcToLoad = "http://www.google.it";
+    let preloadedSrc = srcToLoad + "/doodle";
+
+    beforeEach(() => {
+        iframe = document.createElement("iframe");
+    });
+    test("...with initially empty src", () => {
+        iframe.dataset = {
+            "original": srcToLoad
+        };
+        setSources(iframe, settings);
+        expect(iframe).toHaveAttribute("src", srcToLoad);
+    });
+    test("...with initial value in src", () => {
+        iframe.dataset = {
+            "original": srcToLoad
+        };
+        iframe.setAttribute("src", preloadedSrc);
+        setSources(iframe, settings);
+        expect(iframe).toHaveAttribute("src", srcToLoad);
+    });
+    test("...with initial value in src and empty data-original", () => {
+        iframe.dataset = {
+            "original": ""
+        };
+        iframe.setAttribute("src", preloadedSrc);
+        setSources(iframe, settings);
+        expect(iframe).toHaveAttribute("src", preloadedSrc);
+    });
+});
+
+/*
+describe("_setSources for background image", () => {
+    let i;
+    let testFunct = setSources;
+    let img100 = "http://placehold.it/100x100";
+    let img200 = "http://placehold.it/200x200";
+    let dataSrcPartialAttr = "original";
+    let dataSrcAttr = "data-" + dataSrcPartialAttr;
+    test("...with initially empty style attribute", () => {
+        i = document.createElement("div");
+        i.setAttribute(dataSrcAttr, img200);
+        testFunct(i, "", dataSrcPartialAttr);
+        expect(i.style.backgroundImage).toBe("url(" + img200 + ")");
+    });
+    test("...with initially present style attribute", () => {
+        i = document.createElement("div");
+        i.style.padding = "1px";
+        i.setAttribute(dataSrcAttr, img200);
+        testFunct(i, "", dataSrcPartialAttr);
+        expect(i.style.backgroundImage).toBe("url(" + img200 + ")");
+    });
+    test("...with initially present style and background", () => {
+        i = document.createElement("div");
+        i.style.backgroundImage = "url(" + img100 + ")";
+        i.setAttribute(dataSrcAttr, img200);
+        testFunct(i, "", dataSrcPartialAttr);
+        expect(i.style.backgroundImage).toBe("url(" + img200 + ")");
+    });
+});*/
+
 /*
 describe("setSourcesForPicture", () => {
     let p, s1, s2, i;
@@ -47,113 +180,3 @@ describe("setSourcesForPicture", () => {
     });
 });
 */
-describe("setSources for image", () => {
-    let img, div, settings;
-    let img1 = "http://placehold.it/1x1";
-    let img100 = "http://placehold.it/100x100";
-    let img200 = "http://placehold.it/200x200";
-    let img400 = "http://placehold.it/400x400";
-
-    const _setDataset = (img) => {
-        img.dataset = {
-            "original": img200,
-            "originalSet": img400
-        }
-    };
-
-    const _expects = (img) => {
-        expect(img.getAttribute("src")).toBe(img200);
-        expect(img.getAttribute("srcset")).toBe(img400);
-    };
-
-    beforeEach(() => {
-        // Parent is a div
-        settings = {
-            data_src: "original",
-            data_srcset: "originalSet"
-        }
-        img = document.createElement("img");
-        div = document.createElement("div");
-        div.appendChild(img);
-    });
-
-    test("...with initially empty src and srcset", () => {
-        _setDataset(img);
-        setSources(img, settings);
-        _expects(img);
-    });
-
-    test("...with initial values in src and srcset", () => {
-        img.setAttribute("src", img1);
-        img.setAttribute("srcset", img1);
-        _setDataset(img);
-        setSources(img, settings);
-        _expects(img);
-    });
-    test("...with initial values in src and srcset and empty data-*", () => {
-        img.setAttribute("src", img200);
-        img.setAttribute("srcset", img400);
-        img.dataset = {};
-        setSources(img, settings);
-        _expects(img);
-    });
-});
-/*
-describe("_setSources for iframe", () => {
-    let i;
-    let testFunct = setSources;
-    let srcToLoad = "http://www.google.it";
-    let dataSrcPartialAttr = "original";
-    let dataSrcAttr = "data-" + dataSrcPartialAttr;
-    let srcAttr = "src";
-    beforeEach(() => {
-        i = document.createElement("iframe");
-    });
-    test("...with initially empty src", () => {
-        i.setAttribute(dataSrcAttr, srcToLoad);
-        testFunct(i, "", dataSrcPartialAttr);
-        expect(i.getAttribute("src")).toBe(srcToLoad);
-    });
-    test("...with initial value in src", () => {
-        let newSrc = srcToLoad + "/doodle";
-        i.setAttribute("src", srcToLoad);
-        i.setAttribute(dataSrcAttr, newSrc);
-        testFunct(i, "", dataSrcPartialAttr);
-        expect(i.getAttribute("src")).toBe(newSrc);
-    });
-    test("...with initial value in src and empty data-original", () => {
-        i.setAttribute("src", srcToLoad);
-        i.setAttribute(dataSrcAttr, "");
-        testFunct(i, "", dataSrcPartialAttr);
-        expect(i.getAttribute("src")).toBe(srcToLoad);
-    });
-});
-
-describe("_setSources for background image", () => {
-    let i;
-    let testFunct = setSources;
-    let img100 = "http://placehold.it/100x100";
-    let img200 = "http://placehold.it/200x200";
-    let dataSrcPartialAttr = "original";
-    let dataSrcAttr = "data-" + dataSrcPartialAttr;
-    test("...with initially empty style attribute", () => {
-        i = document.createElement("div");
-        i.setAttribute(dataSrcAttr, img200);
-        testFunct(i, "", dataSrcPartialAttr);
-        expect(i.style.backgroundImage).toBe("url(" + img200 + ")");
-    });
-    test("...with initially present style attribute", () => {
-        i = document.createElement("div");
-        i.style.padding = "1px";
-        i.setAttribute(dataSrcAttr, img200);
-        testFunct(i, "", dataSrcPartialAttr);
-        expect(i.style.backgroundImage).toBe("url(" + img200 + ")");
-    });
-    test("...with initially present style and background", () => {
-        i = document.createElement("div");
-        i.style.backgroundImage = "url(" + img100 + ")";
-        i.setAttribute(dataSrcAttr, img200);
-        testFunct(i, "", dataSrcPartialAttr);
-        expect(i.style.backgroundImage).toBe("url(" + img200 + ")");
-    });
-});*/
