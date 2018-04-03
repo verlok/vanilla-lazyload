eb944e1878cf752a44c543a97b0da116f4c1e2fa
Andrea Verlicchi <verlicchia@yoox.net>
2018-04-03 15:08:47 +0200

    Updated rollup options

--- gulpfile.js
+++ gulpfile.js
@@ -16,9 +16,8 @@ gulp.task("default", function () {
         .pipe(eslint.failAfterError()) // --> failing if errors
         // ----------- rolling up --------------
         .pipe(rollup({
-            format: "umd",
-            moduleName: "LazyLoad",
-            entry: "./src/lazyload.js"
+            input: "./src/lazyload.js",
+            output: { name: "LazyLoad", format: "umd" }
         }))
         .pipe(rename("lazyload.es2015.js"))
         .pipe(gulp.dest(destFolder)) // --> writing rolledup
