8c66859efafe9c69dae0bee5ca5384a8de848ed9
Andrea Verlicchi <andrea.verlicchi@gmail.com>
2018-06-19 22:54:21 +0200

    DRYed with setAttributeIfNotNullOrEmpty

--- src/lazyload.setSources.js
+++ src/lazyload.setSources.js
@@ -23,7 +23,12 @@ export const setSourcesInChildren = function(parentTag, attrName, dataAttrName) 
 export const setSourcesForVideo = function (element, settings) {
     const {data_src: dataSrc} = settings;
     setSourcesInChildren(element, "src", dataSrc);
-}
+};
+
+export const setAttributeIfNotNullOrEmpty = function(element, attrName, value) {
+    if (!value) {return;}
+    element.setAttribute(attrName, value);
+};
 
 export const setSources = function (element, settings) {
     const {data_src: dataSrc, data_srcset: dataSrcSet} = settings;
@@ -31,24 +36,18 @@ export const setSources = function (element, settings) {
     const elementSrc = getData(element, dataSrc);
     if (tagName === "IMG") {
         setSourcesForPicture(element, settings);
-        const imgSrcset = getData(element, dataSrcSet);
-        if (imgSrcset) {
-            element.setAttribute("srcset", imgSrcset);
-        }
-        if (elementSrc) {
-            element.setAttribute("src", elementSrc);
-        }
+        const elementSrcSet = getData(element, dataSrcSet);
+        setAttributeIfNotNullOrEmpty(element, "srcset", elementSrcSet)
+        setAttributeIfNotNullOrEmpty(element, "src", elementSrc)
+        return;
+    }
+    if (tagName === "IFRAME") {
+        setAttributeIfNotNullOrEmpty(element, "src", elementSrc);
         return;
     }
     if (tagName === "VIDEO") {
         setSourcesForVideo(element, settings);
         return; 
-    }
-    if (tagName === "IFRAME") {
-        if (elementSrc) {
-            element.setAttribute("src", elementSrc);
-        }
-        return;
     }
     if (elementSrc) {
         element.style.backgroundImage = `url("${elementSrc}")`;
