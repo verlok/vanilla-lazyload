c80343251ce1ce7ad6cebebdf6f92fd2944b0d7b
Andrea Verlicchi <verlicchia@yoox.net>
2018-07-09 18:14:18 +0200

    Fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,48 @@
 var gulp = require("gulp");
-var uglify = require("gulp-uglify");
 var eslint = require("gulp-eslint");
+var rollup = require("gulp-rollup");
 var rename = require("gulp-rename");
 var babel = require("gulp-babel");
-var rollup = require("gulp-rollup");
+var uglify = require("gulp-uglify");
 
 var destFolder = "./dist";
 
-gulp.task("default", function () {
-    return gulp.src("./src/**/*.js")
-        // ----------- linting --------------
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError())
-        // ----------- rolling up --------------
-        .pipe(rollup({
-            format: "umd",
-            moduleName: "LazyLoad",
-            entry: "./src/lazyload.js"
-        }))
-        .pipe(rename("lazyload.es2015.js"))
-        .pipe(gulp.dest(destFolder)) // --> writing rolledup
-        // ----------- babelizing --------------
-        .pipe(babel({
-            presets: [["es2015", { "modules": false }]],
-            sourceMap: false,
-            plugins: ["transform-object-assign"]
-        }))
-        .pipe(rename("lazyload.js"))
-        .pipe(gulp.dest(destFolder)) // --> writing babelized
-        // ----------- minifying --------------
-        .pipe(uglify())
-        .pipe(rename("lazyload.min.js"))
-        .pipe(gulp.dest(destFolder)); // --> writing uglified
+gulp.task("default", function() {
+	process.env.NODE_ENV = "release";
+	return (
+		gulp.
+			src("./src/**/*.js").
+			// ----------- linting --------------
+			pipe(eslint()).
+			pipe(eslint.format()).
+			pipe(eslint.failAfterError()). // --> failing if errors
+			// ----------- rolling up --------------
+			pipe(
+				rollup({
+					output: { name: "LazyLoad", format: "umd" },
+					input: "./src/lazyload.js"
+				})
+			).
+			pipe(rename("lazyload.es2015.js")).
+			pipe(gulp.dest(destFolder)). // --> writing rolledup
+			// ----------- babelizing --------------
+			pipe(
+				babel({
+					presets: [["es2015", { modules: false }]],
+					sourceMap: false,
+					plugins: ["transform-object-assign"]
+				})
+			).
+			pipe(rename("lazyload.js")).
+			pipe(gulp.dest(destFolder)). // --> writing babelized ES5
+			// ----------- minifying --------------
+			pipe(uglify()).
+			pipe(rename("lazyload.min.js")).
+			pipe(gulp.dest(destFolder)) // --> writing uglified
+	);
 });
 
-gulp.task("watch", function () {
-    gulp.watch("./src/**/*.js", ["default"]);
-    // Other watchers
+gulp.task("watch", function() {
+	gulp.watch("./src/**/*.js", ["default"]);
+	// Other watchers
 });
