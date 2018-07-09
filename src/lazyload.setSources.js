c9f50913301ec4250659b4ae6f01ebaa10eada1e
Andrea Verlicchi <verlicchia@yoox.net>
2018-07-09 18:00:14 +0200

    Added support for lazy sizes attribute

--- src/lazyload.setSources.js
+++ src/lazyload.setSources.js
@@ -1,46 +1,53 @@
-import {getData} from "./lazyload.data";
+import { getData } from "./lazyload.data";
 
 const setSourcesInChildren = function(parentTag, attrName, dataAttrName) {
-    for (let i = 0, childTag; childTag = parentTag.children[i]; i += 1) {
-        if (childTag.tagName === "SOURCE") {
-            let attributeValue = getData(childTag, dataAttrName);
-            if (attributeValue) {
-                childTag.setAttribute(attrName, attributeValue);
-            }
-        }
-    }
+	for (let i = 0, childTag; (childTag = parentTag.children[i]); i += 1) {
+		if (childTag.tagName === "SOURCE") {
+			let attributeValue = getData(childTag, dataAttrName);
+			if (attributeValue) {
+				childTag.setAttribute(attrName, attributeValue);
+			}
+		}
+	}
 };
 
 const setAttributeIfNotNullOrEmpty = function(element, attrName, value) {
-    if (!value) {return;}
-    element.setAttribute(attrName, value);
+	if (!value) {
+		return;
+	}
+	element.setAttribute(attrName, value);
 };
 
-export default function setSources(element, settings) {
-    const dataAttrSrcName = settings.data_src;
-    const elementSrc = getData(element, dataAttrSrcName);
-    const tagName = element.tagName;
-    if (tagName === "IMG") {
-        const dataAttrSrcSetName = settings.data_srcset;
-        const elementSrcSet = getData(element, dataAttrSrcSetName);
-        const parent = element.parentNode;
-        if (parent && parent.tagName === "PICTURE") {
-            setSourcesInChildren(parent, "srcset", dataAttrSrcSetName);
-        }
-        setAttributeIfNotNullOrEmpty(element, "srcset", elementSrcSet);
-        setAttributeIfNotNullOrEmpty(element, "src", elementSrc);
-        return;
-    }
-    if (tagName === "IFRAME") {
-        setAttributeIfNotNullOrEmpty(element, "src", elementSrc);
-        return;
-    }
-    if (tagName === "VIDEO") {
-        setSourcesInChildren(element, "src", dataAttrSrcName);
-        setAttributeIfNotNullOrEmpty(element, "src", elementSrc);
-        return;
-    }
-    if (elementSrc) {
-        element.style.backgroundImage = `url("${elementSrc}")`;
-    }
+export const setSources = function(element, settings) {
+	const {
+		data_sizes: sizesDataName,
+		data_srcset: srcsetDataName,
+		data_src: srcDataName
+	} = settings;
+	const srcDataValue = getData(element, srcDataName);
+	const tagName = element.tagName;
+	if (tagName === "IMG") {
+		const parent = element.parentNode;
+		if (parent && parent.tagName === "PICTURE") {
+			setSourcesInChildren(parent, "srcset", srcsetDataName);
+		}
+		const sizesDataValue = getData(element, sizesDataName);
+		setAttributeIfNotNullOrEmpty(element, "sizes", sizesDataValue);
+		const srcsetDataValue = getData(element, srcsetDataName);
+		setAttributeIfNotNullOrEmpty(element, "srcset", srcsetDataValue);
+		setAttributeIfNotNullOrEmpty(element, "src", srcDataValue);
+		return;
+	}
+	if (tagName === "IFRAME") {
+		setAttributeIfNotNullOrEmpty(element, "src", srcDataValue);
+		return;
+	}
+	if (tagName === "VIDEO") {
+		setSourcesInChildren(element, "src", srcDataName);
+		setAttributeIfNotNullOrEmpty(element, "src", srcDataValue);
+		return;
+	}
+	if (srcDataValue) {
+		element.style.backgroundImage = `url("${srcDataValue}")`;
+	}
 };
