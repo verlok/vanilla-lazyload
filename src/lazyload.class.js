52d94c83f84580d238b8e570f34799c1c144b5bd
Andrea Verlicchi <verlicchia@yoox.net>
2018-07-19 23:03:58 +0200

    Beautified

--- src/lazyload.class.js
+++ src/lazyload.class.js
@@ -1,17 +1,20 @@
-import {supportsClassList} from "./lazyload.environment";
+import { supportsClassList } from "./lazyload.environment";
 
 export const addClass = (element, className) => {
-    if (supportsClassList) {
-        element.classList.add(className);
-        return;
-    }
-    element.className += (element.className ? " " : "") + className;
+	if (supportsClassList) {
+		element.classList.add(className);
+		return;
+	}
+	element.className += (element.className ? " " : "") + className;
 };
 
 export const removeClass = (element, className) => {
-    if (supportsClassList) {
-        element.classList.remove(className);
-        return;
-    }
-    element.className = element.className.replace(new RegExp("(^|\\s+)" + className + "(\\s+|$)"), " ").replace(/^\s+/, "").replace(/\s+$/, "");
+	if (supportsClassList) {
+		element.classList.remove(className);
+		return;
+	}
+	element.className = element.className.
+		replace(new RegExp("(^|\\s+)" + className + "(\\s+|$)"), " ").
+		replace(/^\s+/, "").
+		replace(/\s+$/, "");
 };
