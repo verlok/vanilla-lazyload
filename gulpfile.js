2d9c0a53d516455c41540ad67bafef7d4e5f3c85
Andrea Verlicchi <verlicchia@yoox.net>
2018-08-03 11:21:50 +0200

    Created separated tasks for each output formats

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
 var gulp = require("gulp");
 var eslint = require("gulp-eslint");
 var rollup = require("gulp-rollup");
+var sourcemaps = require("gulp-sourcemaps");
 var rename = require("gulp-rename");
 var babel = require("gulp-babel");
 var uglify = require("gulp-uglify");
 
 var destFolder = "./dist";
 
-gulp.task("default", function() {
+gulp.task("lint", function() {
 	process.env.NODE_ENV = "release";
 	return (
 		gulp.
@@ -15,7 +16,83 @@ gulp.task("default", function() {
 			// ----------- linting --------------
 			pipe(eslint()).
 			pipe(eslint.format()).
-			pipe(eslint.failAfterError()). // --> failing if errors
+			pipe(eslint.failAfterError()) // --> failing if errors
+	);
+});
+
+gulp.task("dist-es", function() {
+	process.env.NODE_ENV = "release";
+	return (
+		gulp.
+			src("./src/**/*.js").
+			// ----------- rolling up --------------
+			pipe(
+				rollup({
+					output: { name: "LazyLoad", format: "es" },
+					input: "./src/lazyload.js"
+				})
+			).
+			pipe(rename("lazyload-es.js")).
+			pipe(gulp.dest(destFolder)) // --> writing rolledup
+	);
+});
+
+gulp.task("dist-cjs", function() {
+	process.env.NODE_ENV = "release";
+	return (
+		gulp.
+			src("./src/**/*.js").
+			pipe(sourcemaps.init()).
+			// ----------- rolling up --------------
+			pipe(
+				rollup({
+					output: { name: "LazyLoad", format: "cjs" },
+					input: "./src/lazyload.js"
+				})
+			).
+			// ----------- babelizing --------------
+			pipe(babel()).
+			pipe(rename("lazyload-cjs.js")).
+			pipe(gulp.dest(destFolder)). // --> writing babelized ES5
+			// ----------- minifying --------------
+			pipe(uglify()).
+			pipe(rename("lazyload-cjs.min.js")).
+			pipe(sourcemaps.write("")). // --> writing sourcemap
+			pipe(gulp.dest(destFolder)) // --> writing uglified
+	);
+});
+
+gulp.task("dist-amd", function() {
+	process.env.NODE_ENV = "release";
+	return (
+		gulp.
+			src("./src/**/*.js").
+			pipe(sourcemaps.init()).
+			// ----------- rolling up --------------
+			pipe(
+				rollup({
+					output: { name: "LazyLoad", format: "amd" },
+					input: "./src/lazyload.js"
+				})
+			).
+			// ----------- babelizing --------------
+			pipe(babel()).
+			pipe(rename("lazyload-amd.js")).
+			pipe(gulp.dest(destFolder)). // --> writing babelized ES5
+			// ----------- minifying --------------
+			pipe(uglify()).
+			pipe(rename("lazyload-amd.min.js")).
+			pipe(sourcemaps.write("")). // --> writing sourcemap
+			pipe(gulp.dest(destFolder)) // --> writing uglified
+	);
+});
+
+gulp.task("dist-umd", function() {
+	process.env.NODE_ENV = "release";
+	return (
+		gulp.
+			src("./src/**/*.js").
+			pipe(sourcemaps.init()).
 			// ----------- rolling up --------------
 			pipe(
 				rollup({
@@ -23,20 +100,24 @@ gulp.task("default", function() {
 					input: "./src/lazyload.js"
 				})
 			).
-			pipe(rename("lazyload.es2015.js")).
-			pipe(gulp.dest(destFolder)). // --> writing rolledup
 			// ----------- babelizing --------------
 			pipe(babel()).
-			pipe(rename("lazyload.js")).
+			pipe(rename("lazyload-umd.js")).
 			pipe(gulp.dest(destFolder)). // --> writing babelized ES5
 			// ----------- minifying --------------
 			pipe(uglify()).
-			pipe(rename("lazyload.min.js")).
+			pipe(rename("lazyload-umd.min.js")).
+			pipe(sourcemaps.write("")). // --> writing sourcemap
 			pipe(gulp.dest(destFolder)) // --> writing uglified
 	);
 });
 
 gulp.task("watch", function() {
-	gulp.watch("./src/**/*.js", ["default"]);
-	// Other watchers
+	gulp.watch("./src/**/*.js", [
+		"lint",
+		"dist-es",
+		"dist-cjs",
+		"dist-amd",
+		"dist-umd"
+	]);
 });
