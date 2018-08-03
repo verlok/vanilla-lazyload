bfc75aff8a671ef581053ac4e0030d7009aef21c
Andrea Verlicchi <verlicchia@yoox.net>
2018-08-03 16:24:37 +0200

    Shortened crawler catching RegExp

--- src/lazyload.environment.js
+++ src/lazyload.environment.js
@@ -1,8 +1,7 @@
 export const runningOnBrowser = typeof window !== "undefined";
 
-export const supportsClassList =
-	runningOnBrowser && "classList" in document.createElement("p");
-
 export const isBot =
 	(runningOnBrowser && !("onscroll" in window)) ||
-	/glebot|bingbot|crawler|spider|robot|crawling/i.test(navigator.userAgent);
+	/(gle|ing|ro)bot|crawl|spider/i.test(navigator.userAgent);
+export const supportsClassList =
+	runningOnBrowser && "classList" in document.createElement("p");
