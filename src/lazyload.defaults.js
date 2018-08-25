43abc711969b7bdf8d659552361898ad70a7784f
Andrea Verlicchi <andrea.verlicchi@gmail.com>
2018-08-25 08:01:40 +0200

    Moved `defaultSettings` definition outside exported function

--- src/lazyload.defaults.js
+++ src/lazyload.defaults.js
@@ -1,21 +1,21 @@
-export default customSettings => {
-	const defaultSettings = {
-		elements_selector: "img",
-		container: document,
-		threshold: 300,
-		data_src: "src",
-		data_srcset: "srcset",
-		data_sizes: "sizes",
-		class_loading: "loading",
-		class_loaded: "loaded",
-		class_error: "error",
-		load_delay: 0,
-		callback_load: null,
-		callback_error: null,
-		callback_set: null,
-		callback_enter: null,
-		to_webp: false
-	};
+const defaultSettings = {
+	elements_selector: "img",
+	container: document,
+	threshold: 300,
+	data_src: "src",
+	data_srcset: "srcset",
+	data_sizes: "sizes",
+	class_loading: "loading",
+	class_loaded: "loaded",
+	class_error: "error",
+	load_delay: 0,
+	callback_load: null,
+	callback_error: null,
+	callback_set: null,
+	callback_enter: null,
+	to_webp: false
+};
 
+export default customSettings => {
 	return Object.assign({}, defaultSettings, customSettings);
 };
