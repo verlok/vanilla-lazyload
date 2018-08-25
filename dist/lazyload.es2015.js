ace0968dfe2eae0b936d91627afc34db5f4b2961
Andrea Verlicchi <andrea.verlicchi@gmail.com>
2018-08-25 08:03:48 +0200

    Re-dist

--- dist/lazyload.es2015.js
+++ dist/lazyload.es2015.js
@@ -1,22 +1,22 @@
-var getInstanceSettings = customSettings => {
-	const defaultSettings = {
-		elements_selector: "img",
-		container: document,
-		threshold: 300,
-		data_src: "src",
-		data_srcset: "srcset",
-		data_sizes: "sizes",
-		class_loading: "loading",
-		class_loaded: "loaded",
-		class_error: "error",
-		load_delay: 0,
-		callback_load: null,
-		callback_error: null,
-		callback_set: null,
-		callback_enter: null,
-		to_webp: false
-	};
+const defaultSettings = {
+	elements_selector: "img",
+	container: document,
+	threshold: 300,
+	data_src: "src",
+	data_srcset: "srcset",
+	data_sizes: "sizes",
+	class_loading: "loading",
+	class_loaded: "loaded",
+	class_error: "error",
+	load_delay: 0,
+	callback_load: null,
+	callback_error: null,
+	callback_set: null,
+	callback_enter: null,
+	to_webp: false
+};
 
+var getInstanceSettings = customSettings => {
 	return Object.assign({}, defaultSettings, customSettings);
 };
 
