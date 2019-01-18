f59a88299c279d86a4715faefd043f3bab6ab6ae
Dmytro Verbovyi <Dmitry.Verbovyy@macys.com>
2019-01-18 18:37:59 +0200

    remove isIntersecting

--- src/lazyload.intersectionObserver.js
+++ src/lazyload.intersectionObserver.js
@@ -1,7 +1,4 @@
-/* entry.isIntersecting needs fallback because is null on some versions of MS Edge, and
-   entry.intersectionRatio is not enough alone because it could be 0 on some intersecting elements */
-export const isIntersecting = entry =>
-	entry.isIntersecting || entry.intersectionRatio > 0;
+export const isIntersecting = entry => entry.intersectionRatio > 0;
 
 export const getObserverSettings = settings => ({
 	root: settings.container === document ? null : settings.container,
