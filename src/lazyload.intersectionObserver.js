53a8ffffd647c27a447009b5d96ac7bf160b581d
verlok <andrea.verlicchi@gmail.com>
2019-02-22 01:14:42 +0100

    Added missing imports

--- src/lazyload.intersectionObserver.js
+++ src/lazyload.intersectionObserver.js
@@ -1,3 +1,6 @@
+import { supportsIntersectionObserver } from "./lazyload.environment";
+import { onEnter, onExit } from "./lazyload.reveal";
+
 /* entry.isIntersecting needs fallback because is null on some versions of MS Edge, and
    entry.intersectionRatio is not enough alone because it could be 0 on some intersecting elements */
 export const isIntersecting = entry =>
