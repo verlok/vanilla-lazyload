f746452d8790ad82e222c77db502c353dbdc26a6
verlok <andrea.verlicchi@gmail.com>
2019-03-04 08:46:21 +0100

    Restored double check `isIntersecting || intersectionRatio > 0`

--- src/lazyload.intersectionObserver.js
+++ src/lazyload.intersectionObserver.js
@@ -1,7 +1,8 @@
 import { supportsIntersectionObserver } from "./lazyload.environment";
 import { onEnter, onExit } from "./lazyload.reveal";
 
-export const isIntersecting = entry => entry.intersectionRatio > 0;
+export const isIntersecting = entry =>
+	entry.isIntersecting || entry.intersectionRatio > 0;
 
 export const getObserverSettings = settings => ({
 	root: settings.container === document ? null : settings.container,
