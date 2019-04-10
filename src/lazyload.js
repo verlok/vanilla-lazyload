edc898658878c5ef90cb9cc406f443ca46de3d29
verlok <andrea.verlicchi@gmail.com>
2019-04-10 23:12:46 +0200

    Even more refactored constructor and update

--- src/lazyload.js
+++ src/lazyload.js
@@ -6,38 +6,32 @@ import { setObserver } from "./lazyload.intersectionObserver";
 import { isBot, runningOnBrowser } from "./lazyload.environment";
 import { shouldUseNative, loadAllNative } from "./lazyload.native";
 
-const nodesetToArray = nodeSet => Array.prototype.slice.call(nodeSet);
+const nodeSetToArray = nodeSet => Array.prototype.slice.call(nodeSet);
 
 const queryElements = settings =>
 	settings.container.querySelectorAll(settings.elements_selector);
 
 const getElements = (elements, settings) =>
-	nodesetToArray(elements || queryElements(settings));
+	purgeProcessedElements(nodeSetToArray(elements || queryElements(settings)));
 
 const LazyLoad = function(customSettings, elements) {
-	const settings = getInstanceSettings(customSettings);
-	this._settings = settings;
+	this._settings = getInstanceSettings(customSettings);
 	this._loadingCount = 0;
-	this._elements = getElements(elements, settings);
 	setObserver(this); // Still useful for elements other than IMG and IFRAME
 	this.update(elements);
 };
 
 LazyLoad.prototype = {
 	update: function(elements) {
-		this._elements = purgeProcessedElements(
-			getElements(elements, this._settings)
-		);
+		var settings = this._settings;
+		this._elements = getElements(elements, settings);
 		if (isBot || !this._observer) {
 			this.loadAll();
 			return;
 		}
-		if (shouldUseNative(settings, true)) {
-			//TODO: Remove force param ^^
+		if (shouldUseNative(settings)) {
 			loadAllNative(this);
-			this._elements = purgeProcessedElements(
-				nodesetToArray(queryElements(this._settings))
-			);
+			this._elements = getElements(elements, settings);
 		}
 		this._elements.forEach(element => {
 			this._observer.observe(element);
@@ -60,8 +54,7 @@ LazyLoad.prototype = {
 	},
 
 	loadAll: function() {
-		var elements = this._elements;
-		elements.forEach(element => {
+		this._elements.forEach(element => {
 			revealAndUnobserve(element, this);
 		});
 	}
