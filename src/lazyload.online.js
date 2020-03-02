20b1649c847482d688b4f122a83490bd90b93613
Andrea Verlicchi <andrea.verlicchi@ynap.com>
2020-03-02 23:13:19 +0100

    Fixed an error on IE11 upon the online event

--- src/lazyload.online.js
+++ src/lazyload.online.js
@@ -1,24 +1,23 @@
 import { runningOnBrowser } from "./lazyload.environment";
 import { resetWasProcessedData } from "./lazyload.data";
 import { removeClass } from "./lazyload.class";
+import { nodeSetToArray } from "./lazyload.nodeset";
 
 export const retryLazyLoad = instance => {
-	var settings = instance._settings;
-	var errorElements = settings.container.querySelectorAll(
-		"." + settings.class_error
-	);
-	errorElements.forEach(element => {
-		removeClass(element, settings.class_error);
-		resetWasProcessedData(element);
-	});
-	instance.update();
+    var settings = instance._settings;
+    var errorElements = settings.container.querySelectorAll("." + settings.class_error);
+    nodeSetToArray(errorElements).forEach(element => {
+        removeClass(element, settings.class_error);
+        resetWasProcessedData(element);
+    });
+    instance.update();
 };
 
 export const setOnlineCheck = instance => {
-	if (!runningOnBrowser) {
-		return;
-	}
-	window.addEventListener("online", event => {
-		retryLazyLoad(instance);
-	});
+    if (!runningOnBrowser) {
+        return;
+    }
+    window.addEventListener("online", event => {
+        retryLazyLoad(instance);
+    });
 };
