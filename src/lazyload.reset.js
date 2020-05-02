7b7cb7e693a5e31df2f708daced626fc03d2c998
Andrea Verlicchi <andrea.verlicchi@ynap.com>
2020-05-02 02:27:39 +0200

    Bugfix & rollback guard against empty instance

--- src/lazyload.reset.js
+++ src/lazyload.reset.js
@@ -3,12 +3,11 @@ import { increaseToLoadCount } from "./lazyload.load";
 import { decreaseLoadingCount } from "./lazyload.event";
 
 export const resetElement = (element, instance) => {
-    setStatus(element, null);
-    if (!instance) return;
     if (hasStatusAfterLoading(element)) {
         increaseToLoadCount(instance);
     }
     if (hasStatusLoading(element)) {
         decreaseLoadingCount(instance);
     }
+    setStatus(element, null);
 };
