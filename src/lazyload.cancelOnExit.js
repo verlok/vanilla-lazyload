88d01c31b109b1b6ad271e473f084e84a3b9d070
Andrea Verlicchi <andrea.verlicchi@ynap.com>
2020-05-06 02:32:59 +0200

    Removing "loading" class when canceled on exit

--- src/lazyload.cancelOnExit.js
+++ src/lazyload.cancelOnExit.js
@@ -2,6 +2,7 @@ import { hasStatusLoading } from "./lazyload.data";
 import { decreaseLoadingCount, removeEventListeners } from "./lazyload.event";
 import { resetSourcesImg } from "./lazyload.setSources";
 import { safeCallback } from "./lazyload.callback";
+import { removeClass } from "./lazyload.class";
 
 export const cancelIfLoading = (element, entry, settings, instance) => {
     if (!settings.cancel_on_exit) return;
@@ -9,6 +10,7 @@ export const cancelIfLoading = (element, entry, settings, instance) => {
     if (element.tagName !== "IMG") return;
     removeEventListeners(element);
     resetSourcesImg(element, settings, instance);
+    removeClass(element, settings.class_loading);
     decreaseLoadingCount(instance);
     safeCallback(settings.callback_cancel, element, entry, instance);
     // setTimeout is needed because the "callback_cancel" implementation
