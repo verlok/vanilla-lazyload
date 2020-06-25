c4bd4237e44d54707bdcae8bcb975e505c64ede0
Andrea Verlicchi <andrea.verlicchi@ynap.com>
2020-06-25 16:00:42 +0200

    Refactoring method names

--- src/lazyload.intersectionHandlers.js
+++ src/lazyload.intersectionHandlers.js
@@ -1,18 +1,18 @@
 import { safeCallback } from "./lazyload.callback";
 import { load } from "./lazyload.load";
 import { hasEmptyStatus, hadStartedLoading } from "./lazyload.data";
-import { cancelLoadingIfRequired } from "./lazyload.cancelOnExit";
-import { unobserveIfRequired } from "./lazyload.unobserve";
+import { cancelLoading } from "./lazyload.cancelOnExit";
+import { unobserveEntered } from "./lazyload.unobserve";
 
 export const onEnter = (element, entry, settings, instance) => {
     safeCallback(settings.callback_enter, element, entry, instance);
-    unobserveIfRequired(element, settings, instance);
+    unobserveEntered(element, settings, instance);
     if (hadStartedLoading(element)) return; //Prevent loading it again
     load(element, settings, instance);
 };
 
 export const onExit = (element, entry, settings, instance) => {
     if (hasEmptyStatus(element)) return; //Ignore the first pass, at landing
-    cancelLoadingIfRequired(element, entry, settings, instance);
+    cancelLoading(element, entry, settings, instance);
     safeCallback(settings.callback_exit, element, entry, instance);
 };
