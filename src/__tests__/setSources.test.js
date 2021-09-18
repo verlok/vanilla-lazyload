b008e529c1e4e8c2a397ee78765ee49b705f89c7
verlok <andrea.verlicchi@gmail.com>
2021-09-19 00:19:56 +0200

    Removed all "..." from test("...")

--- src/__tests__/setSources.test.js
+++ src/__tests__/setSources.test.js
@@ -28,7 +28,7 @@ describe("setSources for image", () => {
     let img200 = "200.gif";
     let img400 = "400.gif";
 
-    test("...with initially empty src and srcset", () => {
+    test("with initially empty src and srcset", () => {
         img.setAttribute("data-src", img200);
         img.setAttribute("data-srcset", img400);
         setSources(img, settings, getFakeInstance());
@@ -36,7 +36,7 @@ describe("setSources for image", () => {
         expect(img).toHaveAttributeValue("srcset", img400);
     });
 
-    test("...with initial values in src and srcset", () => {
+    test("with initial values in src and srcset", () => {
         img.setAttribute("data-src", img200);
         img.setAttribute("data-srcset", img400);
         img.setAttribute("src", img1);
@@ -45,7 +45,7 @@ describe("setSources for image", () => {
         expect(img).toHaveAttributeValue("src", img200);
         expect(img).toHaveAttributeValue("srcset", img400);
     });
-    test("...with initial values in src and srcset and empty data-*", () => {
+    test("with initial values in src and srcset and empty data-*", () => {
         img.setAttribute("data-src", "");
         img.setAttribute("data-srcset", "");
         img.setAttribute("src", img200);
@@ -64,18 +64,18 @@ describe("setSources for iframe", () => {
     beforeEach(() => {
         iframe = document.createElement("iframe");
     });
-    test("...with initially empty src", () => {
+    test("with initially empty src", () => {
         iframe.setAttribute("data-src", srcToLoad);
         setSources(iframe, settings, getFakeInstance());
         expect(iframe).toHaveAttributeValue("src", srcToLoad);
     });
-    test("...with initial value in src", () => {
+    test("with initial value in src", () => {
         iframe.setAttribute("data-src", srcToLoad);
         iframe.setAttribute("src", preloadedSrc);
         setSources(iframe, settings, getFakeInstance());
         expect(iframe).toHaveAttributeValue("src", srcToLoad);
     });
-    test("...with initial value in src and empty data-src", () => {
+    test("with initial value in src and empty data-src", () => {
         iframe.setAttribute("data-src", "");
         iframe.setAttribute("src", preloadedSrc);
         setSources(iframe, settings, getFakeInstance());
@@ -93,13 +93,13 @@ describe("setBackground for single background image", () => {
         element.llTempImage = document.createElement("img");
     });
 
-    test("...with initially empty style attribute", () => {
+    test("with initially empty style attribute", () => {
         element.setAttribute("data-bg", img200);
         setBackground(element, settings, getFakeInstance());
         // Test cheating: bug in JsDOM doesn't return the url("") with quotes inside
         expect(element.style.backgroundImage).toBe(`url(${img200})`);
     });
-    test("...with initially present style attribute", () => {
+    test("with initially present style attribute", () => {
         element.setAttribute("data-bg", img100);
         element.style = {
             padding: "1px"
@@ -108,7 +108,7 @@ describe("setBackground for single background image", () => {
         // Test cheating: bug in JsDOM doesn't return the url("") with quotes inside
         expect(element.style.backgroundImage).toBe(`url(${img100})`);
     });
-    test("...with initially present style and background", () => {
+    test("with initially present style and background", () => {
         element.setAttribute("data-bg", img200);
         element.style = {
             padding: "1px",
@@ -129,13 +129,13 @@ describe("setMultiBackground for multiple background image", () => {
         element = document.createElement("div");
     });
 
-    test("...with initially empty style attribute", () => {
+    test("with initially empty style attribute", () => {
         element.setAttribute("data-bg-multi", `url(${img200})`);
         setMultiBackground(element, settings, getFakeInstance());
         // Test cheating: bug in JsDOM doesn't return the url("") with quotes inside
         expect(element.style.backgroundImage).toBe(`url(${img200})`);
     });
-    test("...with initially present style attribute", () => {
+    test("with initially present style attribute", () => {
         element.setAttribute("data-bg-multi", `url(${img100})`);
         element.style = {
             padding: "1px"
@@ -144,7 +144,7 @@ describe("setMultiBackground for multiple background image", () => {
         // Test cheating: bug in JsDOM doesn't return the url("") with quotes inside
         expect(element.style.backgroundImage).toBe(`url(${img100})`);
     });
-    test("...with initially present style and background", () => {
+    test("with initially present style and background", () => {
         element.setAttribute("data-bg-multi", `url(${img200})`);
         element.style = {
             padding: "1px",
@@ -168,7 +168,7 @@ describe("setSources for video", () => {
         video.appendChild(document.createElement("source"));
     });
 
-    test("...with initially empty src", () => {
+    test("with initially empty src", () => {
         video.load = jest.fn();
         video.setAttribute("data-src", videoUrlAvi);
         setSources(video, settings, getFakeInstance());
@@ -176,7 +176,7 @@ describe("setSources for video", () => {
         expect(video.load).toHaveBeenCalled();
     });
 
-    test("...with source elements", () => {
+    test("with source elements", () => {
         video.load = jest.fn();
         video.setAttribute("data-src", videoUrlAvi);
         video.appendChild((source1 = document.createElement("source")));
@@ -204,7 +204,7 @@ describe("setSources for picture", () => {
         picture.appendChild((img = document.createElement("img")));
     });
 
-    test("...with initially empty srcset", () => {
+    test("with initially empty srcset", () => {
         source1.setAttribute("data-srcset", img200);
         source2.setAttribute("data-srcset", img400);
         setSources(img, settings, getFakeInstance());
@@ -212,7 +212,7 @@ describe("setSources for picture", () => {
         expect(source2).toHaveAttributeValue("srcset", img400);
     });
 
-    test("...with initial value in srcset", () => {
+    test("with initial value in srcset", () => {
         source1.setAttribute("data-srcset", img200);
         source2.setAttribute("data-srcset", img400);
         source1.setAttribute("srcset", img1);
@@ -222,7 +222,7 @@ describe("setSources for picture", () => {
         expect(source2).toHaveAttributeValue("srcset", img400);
     });
 
-    test("...with initial value in srcset and empty data-srcset", () => {
+    test("with initial value in srcset and empty data-srcset", () => {
         source1.setAttribute("data-srcset", "");
         source2.setAttribute("data-srcset", "");
         source1.setAttribute("srcset", img200);
@@ -265,7 +265,7 @@ describe("resetSources for image", () => {
     let img200 = "200.gif";
     let img400 = "400.gif";
 
-    test("...with initially empty src and srcset", () => {
+    test("with initially empty src and srcset", () => {
         img.setAttribute("data-src", img200);
         img.setAttribute("data-srcset", img400);
         setSources(img, settings, getFakeInstance());
@@ -274,7 +274,7 @@ describe("resetSources for image", () => {
         expect(img).not.toHaveAttributeValue("srcset");
     });
 
-    /* test("...with initial values in src and srcset", () => {
+    /* test("with initial values in src and srcset", () => {
         img.setAttribute("data-src", img200);
         img.setAttribute("data-srcset", img400);
         img.setAttribute("src", img1);
@@ -284,7 +284,7 @@ describe("resetSources for image", () => {
         expect(img).toHaveAttributeValue("src", img1);
         expect(img).toHaveAttributeValue("srcset", img2);
     });
-    test("...with initial values in src and srcset and empty data-*", () => {
+    test("with initial values in src and srcset and empty data-*", () => {
         img.setAttribute("data-src", "");
         img.setAttribute("data-srcset", "");
         img.setAttribute("src", img200);
@@ -304,20 +304,20 @@ describe("resetSources for image", () => {
     beforeEach(() => {
         iframe = document.createElement("iframe");
     });
-    test("...with initially empty src", () => {
+    test("with initially empty src", () => {
         iframe.setAttribute("data-src", srcToLoad);
         setSources(iframe, settings, getFakeInstance());
         resetSources(iframe);
         expect(iframe).toHaveAttributeValue("src", srcToLoad);
     });
-    test("...with initial value in src", () => {
+    test("with initial value in src", () => {
         iframe.setAttribute("data-src", srcToLoad);
         iframe.setAttribute("src", preloadedSrc);
         setSources(iframe, settings, getFakeInstance());
         resetSources(iframe);
         expect(iframe).toHaveAttributeValue("src", srcToLoad);
     });
-    test("...with initial value in src and empty data-src", () => {
+    test("with initial value in src and empty data-src", () => {
         iframe.setAttribute("data-src", "");
         iframe.setAttribute("src", preloadedSrc);
         setSources(iframe, settings, getFakeInstance());
@@ -336,13 +336,13 @@ describe("resetBackground for single background image", () => {
         element.llTempImage = document.createElement("img");
     });
 
-    test("...with initially empty style attribute", () => {
+    test("with initially empty style attribute", () => {
         element.setAttribute("data-bg", img200);
         setBackground(element, settings, getFakeInstance());
         // Test cheating: bug in JsDOM doesn't return the url("") with quotes inside
         expect(element.style.backgroundImage).toBe(`url(${img200})`);
     });
-    test("...with initially present style attribute", () => {
+    test("with initially present style attribute", () => {
         element.setAttribute("data-bg", img100);
         element.style = {
             padding: "1px"
@@ -351,7 +351,7 @@ describe("resetBackground for single background image", () => {
         // Test cheating: bug in JsDOM doesn't return the url("") with quotes inside
         expect(element.style.backgroundImage).toBe(`url(${img100})`);
     });
-    test("...with initially present style and background", () => {
+    test("with initially present style and background", () => {
         element.setAttribute("data-bg", img200);
         element.style = {
             padding: "1px",
@@ -372,13 +372,13 @@ describe("resetMultiBackground for multiple background image", () => {
         element = document.createElement("div");
     });
 
-    test("...with initially empty style attribute", () => {
+    test("with initially empty style attribute", () => {
         element.setAttribute("data-bg-multi", `url(${img200})`);
         setMultiBackground(element, settings, getFakeInstance());
         // Test cheating: bug in JsDOM doesn't return the url("") with quotes inside
         expect(element.style.backgroundImage).toBe(`url(${img200})`);
     });
-    test("...with initially present style attribute", () => {
+    test("with initially present style attribute", () => {
         element.setAttribute("data-bg-multi", `url(${img100})`);
         element.style = {
             padding: "1px"
@@ -387,7 +387,7 @@ describe("resetMultiBackground for multiple background image", () => {
         // Test cheating: bug in JsDOM doesn't return the url("") with quotes inside
         expect(element.style.backgroundImage).toBe(`url(${img100})`);
     });
-    test("...with initially present style and background", () => {
+    test("with initially present style and background", () => {
         element.setAttribute("data-bg-multi", `url(${img200})`);
         element.style = {
             padding: "1px",
@@ -411,7 +411,7 @@ describe("resetSources for video", () => {
         video.appendChild(document.createElement("source"));
     });
 
-    test("...with initially empty src", () => {
+    test("with initially empty src", () => {
         video.load = jest.fn();
         video.setAttribute("data-src", videoUrlAvi);
         setSources(video, settings, getFakeInstance());
@@ -420,7 +420,7 @@ describe("resetSources for video", () => {
         expect(video.load).toHaveBeenCalled();
     });
 
-    test("...with source elements", () => {
+    test("with source elements", () => {
         video.load = jest.fn();
         video.setAttribute("data-src", videoUrlAvi);
         video.appendChild((source1 = document.createElement("source")));
@@ -449,7 +449,7 @@ describe("resetSources for picture", () => {
         picture.appendChild((img = document.createElement("img")));
     });
 
-    test("...with initially empty srcset", () => {
+    test("with initially empty srcset", () => {
         source1.setAttribute("data-srcset", img200);
         source2.setAttribute("data-srcset", img400);
         setSources(img, settings, getFakeInstance());
@@ -458,7 +458,7 @@ describe("resetSources for picture", () => {
         expect(source2).toHaveAttributeValue("srcset", img400);
     });
 
-    test("...with initial value in srcset", () => {
+    test("with initial value in srcset", () => {
         source1.setAttribute("data-srcset", img200);
         source2.setAttribute("data-srcset", img400);
         source1.setAttribute("srcset", img1);
@@ -469,7 +469,7 @@ describe("resetSources for picture", () => {
         expect(source2).toHaveAttributeValue("srcset", img400);
     });
 
-    test("...with initial value in srcset and empty data-srcset", () => {
+    test("with initial value in srcset and empty data-srcset", () => {
         source1.setAttribute("data-srcset", "");
         source2.setAttribute("data-srcset", "");
         source1.setAttribute("srcset", img200);
