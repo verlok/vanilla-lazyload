6606d93d743ff3d95b60dd323fb4e06fd29df78c
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2023-10-15 10:07:55 +0530

    Update callback.js
    Improving the callback check.

--- src/callback.js
+++ src/callback.js
@@ -1,15 +1,12 @@
 export const safeCallback = (callback, arg1, arg2, arg3) => {
-  if (!callback) {
-    return;
-  }
-
+  if (typeof callback !== 'function') return;
   if (arg3 !== undefined) {
     callback(arg1, arg2, arg3);
-    return;
-  }
-  if (arg2 !== undefined) {
+  } else if (arg2 !== undefined) {
     callback(arg1, arg2);
-    return;
+  } else {
+    callback(arg1);
   }
-  callback(arg1);
-};
+}
+
+ 
