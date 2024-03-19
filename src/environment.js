f497b37f39d23ee29c8409522a0ca36c4e79e423
Andrea Verlicchi <andrea.verlicchi@gmail.com>
2024-03-19 16:09:10 +0100

    Assuming IntersectionObserver is there. This drops support to IE 11 too.

--- src/environment.js
+++ src/environment.js
@@ -4,6 +4,4 @@ export const isBot =
   (runningOnBrowser && !("onscroll" in window)) ||
   (typeof navigator !== "undefined" && /(gle|ing|ro)bot|crawl|spider/i.test(navigator.userAgent));
 
-export const supportsIntersectionObserver = runningOnBrowser && "IntersectionObserver" in window;
-
 export const isHiDpi = runningOnBrowser && window.devicePixelRatio > 1;
