b1e13b8c16a6a67effa177e1b66eccd1d899f699
Andrea Verlicchi <andrea.verlicchi@gmail.com>
2024-04-02 08:33:37 +0200

    Removed comment block, improved readability

--- tests/e2e/image_basic.spec.js
+++ tests/e2e/image_basic.spec.js
@@ -1,21 +1,16 @@
 // @ts-check
 import { expect, test } from "@playwright/test";
 
-test("Esm webpage test", async ({ page }) => {
+test("Basic usage", async ({ page }) => {
   await page.goto("/demos/image_basic.html");
-
-/*  page.on('console', async (msg) => {
-    const msgArgs = msg.args();
-    const logValues = await Promise.all(msgArgs.map(async arg => await arg.jsonValue()));
-    console.log(...logValues);
-  });*/
 
   // Find all images with data-src attribute
   const lazyLoadImages = await page.locator('img[data-src]');
 
+  // Wait for page loaded
   await page.waitForLoadState("load");
 
-// For each image with data-src attribute, scroll into view and check if it loads
+  // For each image with data-src attribute, scroll into view and check if it loads
   const imageCount = await lazyLoadImages.count()
 
   for (let i = 0; i < imageCount; i++) {
@@ -25,6 +20,7 @@ test("Esm webpage test", async ({ page }) => {
     await image.scrollIntoViewIfNeeded();
 
     // Wait for lazy loaded image to have a src attribute
-    await expect(image).toHaveAttribute('src', await image.getAttribute('data-src'));
+    const expected = await image.getAttribute('data-src');
+    await expect(image).toHaveAttribute('src', expected);
   }
 });
