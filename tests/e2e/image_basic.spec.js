d2862646401209b97426fb22dd69f677c60cba79
Andrea Verlicchi <andrea.verlicchi@gmail.com>
2024-04-02 16:52:35 +0200

    One test file for multiple demos of simple images

--- tests/e2e/image_basic.spec.js
+++ tests/e2e/image_basic.spec.js
@@ -1,18 +1,28 @@
 import { expect, test } from "@playwright/test";
+const limit = 10;
 
-test("Basic image", async ({ page }) => {
-  await page.goto("/demos/image_basic.html");
-  const lazyLoadImages = await page.locator('img[data-src]');
-  await page.waitForLoadState("load");
-  const imageCount = await lazyLoadImages.count()
-  
-  // Eventually scroll into view and check if it loads
-  for (let i = 0; i < imageCount; i++) {
-    const image = lazyLoadImages.nth(i);
-    await image.scrollIntoViewIfNeeded();
+const pagesWithSimpleImages = [
+  { url: "/demos/image_basic.html", description: "Basic page" },
+  { url: "/demos/async.html", description: "Async initialization" }
+];
 
-    // Check the src attribute
-    const expectedSrc = await image.getAttribute('data-src');
-    await expect(image).toHaveAttribute('src', expectedSrc);
-  }
-});
+for (const { url, description } of pagesWithSimpleImages) {
+  test(description, async ({ page }) => {
+    await page.goto(url);
+    const lazyLoadImages = await page.locator("img[data-src]");
+    await page.waitForLoadState("load");
+    const imageCount = limit //was: await lazyLoadImages.count();
+
+    // Eventually scroll into view and check if it loads
+    for (let i = 0; i < imageCount; i++) {
+      const image = lazyLoadImages.nth(i);
+      await image.scrollIntoViewIfNeeded();
+
+      // Check the src attribute
+      const expectedSrc = await image.getAttribute("data-src");
+      await expect(image).toHaveAttribute("src", expectedSrc);
+    }
+  });
+
+  // You can also do it with test.describe() or with multiple tests as long the test name is unique.
+}
