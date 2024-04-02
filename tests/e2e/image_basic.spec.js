1b58a7415ac2063054d7c5b7b78974807dc9403a
Andrea Verlicchi <andrea.verlicchi@gmail.com>
2024-04-02 16:15:48 +0200

    Simplified e2e spec file

--- tests/e2e/image_basic.spec.js
+++ tests/e2e/image_basic.spec.js
@@ -1,26 +1,18 @@
-// @ts-check
 import { expect, test } from "@playwright/test";
 
-test("Basic usage", async ({ page }) => {
+test("Basic image", async ({ page }) => {
   await page.goto("/demos/image_basic.html");
-
-  // Find all images with data-src attribute
   const lazyLoadImages = await page.locator('img[data-src]');
-
-  // Wait for page loaded
   await page.waitForLoadState("load");
-
-  // For each image with data-src attribute, scroll into view and check if it loads
   const imageCount = await lazyLoadImages.count()
-
+  
+  // Eventually scroll into view and check if it loads
   for (let i = 0; i < imageCount; i++) {
     const image = lazyLoadImages.nth(i);
-
-    // Scroll to the image
     await image.scrollIntoViewIfNeeded();
 
-    // Wait for lazy loaded image to have a src attribute
-    const expected = await image.getAttribute('data-src');
-    await expect(image).toHaveAttribute('src', expected);
+    // Check the src attribute
+    const expectedSrc = await image.getAttribute('data-src');
+    await expect(image).toHaveAttribute('src', expectedSrc);
   }
 });
